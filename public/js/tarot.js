b8b4327e5a62cbfbbe9e9abf32dac46efc7b43f0
李光軒 <light0617@liguanghande-MacBook-Pro.local>
2018-04-16 22:04:27 -0700

    update flip

--- public/js/tarot.js
+++ public/js/tarot.js
@@ -1,6 +1,9 @@
 var app = angular.module('myApp', []);
+
 app.controller('MainController', ctrlFunc);
 function ctrlFunc(){
+    this.positiveCards = [];
+    this.negativeCards = [];
     this.cards = clientCards;
     this.range = function(n) {
         return new Array(n);
@@ -11,5 +14,23 @@ function ctrlFunc(){
     }
     this.get_rows = function(cards){
         return Math.ceil(cards.length / 4);
+    }
+    this.incrementSelectCount = function(selectCount, card){
+        selectCount = (selectCount+1) % 3;
+        if(selectCount == 1){
+            this.positiveCards.push(card);
+        }else if(selectCount == 2){
+            var index = this.positiveCards.indexOf(card);
+            if(index > -1){
+                this.positiveCards.splice(index, 1);
+            }
+            this.negativeCards.push(card);
+        }else{
+            var index = this.negativeCards.indexOf(card);
+            if(index > -1){
+                this.negativeCards.splice(index, 1);
+            }
+        }
+        return selectCount;
     }
 }
